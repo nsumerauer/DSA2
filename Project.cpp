ced1d2b25bba5dfde193bf975a5305060247d34d
nsumerauer <n-sumerauer@onu.edu>
2025-11-03 15:00:39 -0500

    Base stuff added

--- Project.cpp
+++ Project.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <algorithm>
+#include <chrono>
 using namespace std;
+
+int linear_search(int arr[]);
+int binary_search(int arr[]);
+
 void main()
 {
+    int loc = -1;
+    const auto start{std::chrono::steady_clock::now()};
+
+    const auto finish{std::chrono::steady_clock::now()};
+}
+
+int linear_search(int arr[]){
+
+}
+
+int binary_search(int arr[]){
     
 }
